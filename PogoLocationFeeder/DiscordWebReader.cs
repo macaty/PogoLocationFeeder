306816bdabb5ecd616216f338a303cc9ac1db43b
Ilses <dfoilses+git@gmail.com>
2016-08-01 15:01:53 -0700

    fixup! fixup! Initial commit for new Discord integration

--- PogoLocationFeeder/DiscordWebReader.cs
+++ PogoLocationFeeder/DiscordWebReader.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Net;
 using System.Text;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace PogoLocationFeeder.DiscordWebReader
 {
@@ -12,50 +13,85 @@ namespace PogoLocationFeeder.DiscordWebReader
 
         public DiscordWebReader()
         {
-            InitialiseWebClient();
+            InitializeWebClient();
         }
 
-        // When SSE (Server side event) occurs this fires
-        private void OnOpenReadCompleted(object sender, OpenReadCompletedEventArgs args)
+        private void InitializeWebClient()
         {
-            using (var streamReader = new StreamReader(args.Result, Encoding.UTF8))
+            var request = WebRequest.Create(new Uri("http://138.68.22.176/messsages"));
+            ((HttpWebRequest)request).AllowReadStreamBuffering = false;
+            var response = request.GetResponse();
+            var stream = response.GetResponseStream();
+
+            ReadStreamForever(stream);
+        }
+        public static void ReadStreamForever(Stream stream)
+        {
+            var encoder = new UTF8Encoding();
+            var buffer = new byte[2048];
+            while (true)
             {
-                var serverPayload = streamReader.ReadLine();
+                //TODO: Better evented handling of the response stream
 
-                Console.WriteLine("Raw message: {0}", serverPayload.ToString());
-                var message = JsonConvert.DeserializeObject<Message>(serverPayload);
-                if (message != null)
+                if (stream.CanRead)
                 {
-                    Console.WriteLine("Discord message received: {0}", message.content);
+                    int len = stream.Read(buffer, 0, 2048);
+                    if (len > 0)
+                    {
+                        var serverPayload = encoder.GetString(buffer, 0, len);
+                        //Console.WriteLine("text={0}", serverPayload);
+
+                        try
+                        {
+                            var message = serverPayload.Split(new[] { '\r', '\n' })[2];
+                            if (message.Length == 0) continue;
+
+                            var jsonPayload = message.Substring(5);
+                            //Console.WriteLine($"JSON: {jsonPayload}");
+
+                            var result = JsonConvert.DeserializeObject<Message>(jsonPayload);
+                            if (result != null)
+                            {
+                                Console.WriteLine("Discord message received: {0}", result.content);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Exception: {e.ToString()}\n\n\n");
+                        }
+                        
+
+                        // TODO: relayMessageToClients(message.content, "Discord");
+                        //Application.Push(text);
+                    }
                 }
-
-                // TODO: relayMessageToClients(message.content, "Discord");
-
-                InitialiseWebClient();
+                //System.Threading.Thread.Sleep(250);
             }
         }
-
-        private void InitialiseWebClient()
-        {
-            wc = new WebClient();
-            wc.OpenReadAsync(new Uri("http://138.68.22.176/messsages"));
-            wc.OpenReadCompleted += OnOpenReadCompleted;
-        }
     }
-
+    public class AuthorStruct
+    {
+        public string username;
+        public string id;
+        public string discriminator;
+        public string avatar;
+    }
     public class Message
     {
         public string id = "";
-        public string channel_id ="";
-        public string author = "";
+        public string channel_id = "";
+        //public List<AuthorStruct> author;
         public string content = "";
         public string timestamp = "";
-        public string edited_timestamp = "";
-        public string tts = "";
-        public string mention_everyone = "";
-        public string mentions = "";
-        public string mention_roles = "";
-        public string attachments = "";
-        public string embeds = "";
+        //public string edited_timestamp = null;
+        public bool tts = false;
+        //public bool mention_everyone = false;
+        //public bool pinned = false;
+        //public bool deleted = false;
+        //public string nonce = "";
+        //public string mentions = "";
+        //public string mention_roles = "";
+        //public xxx attachments = "";
+        //public xxx embeds = "";
     }
 }
